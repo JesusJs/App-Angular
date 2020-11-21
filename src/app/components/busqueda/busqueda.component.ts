75dcdaae6682e884a25dc4893a779b48b9fd64be
Jesus Chavez <jdchavezzss@gmail.com>
2020-11-21 00:46:38 -0400

    version 1

---
+++ src/app/components/busqueda/busqueda.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
+
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html'
+})
+export class BusquedaComponent implements OnInit {
+
+    heroes:any[] = []
+    termino:string;
+
+  constructor( private activatedRoute:ActivatedRoute,
+               private _heroesService: HeroesService
+                ) {
+
+
+  }
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe( params =>{
+      // tslint:disable-next-line: no-string-literal
+      this.termino =params['termino'];
+      this.heroes = this._heroesService.buscarHeroes( params['termino'] );
+      console.log( this.heroes );
+    });
+
+  }
+
+}
